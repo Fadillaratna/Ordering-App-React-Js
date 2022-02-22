e7e493c953b546593e4a9dc6dc551faa801d6b11
FadillaRatnaDwita29RPL <fadilladwita20@gmail.com>
2022-02-22 22:32:37 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {Hasil, ListCategories, NavbarComponent} from './component';
+import { Row, Col, Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarComponent />
+      <div className="mt-3">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4 className="fw-bold fs-4">Products</h4>
+              <hr />
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
