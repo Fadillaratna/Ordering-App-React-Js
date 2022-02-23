4f631bbbf5c2b7125caefd3e93d950e898ce5c3e
FadillaRatnaDwita29RPL <fadilladwita20@gmail.com>
2022-02-23 18:00:02 +0700

    first commit

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Col, Card } from "react-bootstrap";
+import { Col, Card, Button } from "react-bootstrap";
 import { numberWithCommas } from "../utils/utils";
 
 const Menus = ({ menu, masukKeranjang }) => {
   return (
     <Col md={4} xs={6} className="mb-4 py-3">
-      <Card className="shadow" id="p">
+      <Card className="shadow" id="p" onClick={() => masukKeranjang(menu)}>
         <Card.Img
           variant="top"
           src={
@@ -18,7 +18,9 @@ const Menus = ({ menu, masukKeranjang }) => {
         <Card.Body>
           <Card.Title>{menu.nama} <strong>({menu.kode})</strong></Card.Title>
           <Card.Text>Rp. {numberWithCommas(menu.harga)}</Card.Text>
-          <button onClick={() => masukKeranjang(menu)} class="btn btn-outline-dark">Add To Cart</button>
+        </Card.Body>
+        <Card.Body>
+          <Button onClick={() => masukKeranjang(menu)} class="btn btn-outline-dark">Add To Cart</Button>
         </Card.Body>
       </Card>
     </Col>
